36e964eae1d65e2986c505cafe04f5410815a4ec
ZikXewen <datasup12@gmail.com>
2025-08-07 10:05:00 -0500

    use nixos-unstable

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS Config Flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
