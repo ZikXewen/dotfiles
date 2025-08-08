af7c29b60637e24e5511669974db536521136bf3
ZikXewen <datasup12@gmail.com>
2025-08-08 16:23:38 -0500

    some fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,14 @@
 if status is-interactive
   set fish_greeting
   starship init fish | source
+  alias lg lazygit
+  alias dcd "docker compose down"
+  alias dcu "docker compose up --detach"
+  alias dcub "dcu --build"
+end
+
+if status is-login 
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    dbus-run-session hyprland
+  end
 end
